1a5b47b23726abb253881a386813ff2f978f6074
4rix-co4es <ajirobe84@gmail.com>
2025-08-31 06:26:10 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box");
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.contentEditable = "true";
+    inputBox.className = "input-box";
+    inputBox.innerHTML = '<img src="images/delete.png" alt="">';
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(note => {
+            note.onkeyup = function () {
+                updateStorage();
+            }
+        });
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.key == "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
+
+
